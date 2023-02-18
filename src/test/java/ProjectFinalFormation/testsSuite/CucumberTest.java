89685ba54567d76f8eb49582d46629922b87d4c0
vin <vincent.paya@gmail.com>
2023-02-18 14:07:43 +0100

    add json test output and adapt java requirement for Jenkins

--- src/test/java/ProjectFinalFormation/testsSuite/CucumberTest.java
+++ src/test/java/ProjectFinalFormation/testsSuite/CucumberTest.java
@@ -1,6 +1,7 @@
 package ProjectFinalFormation.testsSuite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -11,6 +12,7 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("ProjectFinalFormation/testsSuite")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ProjectFinalFormation.testsSuite")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber.json")
 public class CucumberTest {
 
 }
